b7b0f75c77ab87c0a04312f5c0ff35db2f054089
Viktor Velychkocd <velviktor831@gmail.com>
2026-04-25 10:58:19 +0300

    feat: add initial database models

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name="plays")
+    actors = models.ManyToManyField(Actor, related_name="plays")
+
+    def __str__(self):
+        return self.title
