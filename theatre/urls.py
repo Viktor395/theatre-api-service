2ff4beea6abcd6fab1a5ab56fb9112fea29975fe
Viktor Velychkocd <velviktor831@gmail.com>
2026-04-25 13:04:05 +0300

    "feat: add docker configuration"

---
+++ theatre/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+from theatre.views import ActorViewSet, GenreViewSet, PlayViewSet
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("plays", PlayViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "theatre"
