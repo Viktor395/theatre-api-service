454e8ff430bfc2e89a998c8eabb183d4813820ee
Viktor Velychkocd <velviktor831@gmail.com>
2026-04-25 12:44:12 +0300

    feat: add ViewSets for Actor, Genre, and Play

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from theatre.models import Actor, Genre, Play
+from theatre.serializers import ActorSerializer, GenreSerializer, PlaySerializer
 
-# Create your views here.
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
